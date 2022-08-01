5fd8ccf767d9b328bd324caa12d9a83009ba34b8
Cleo Aguiar <cleotaisas@gmail.com>
2022-08-01 20:57:20 -0300

    feat: PROBLEMA: 1000 - Hello World!

---
+++ 1.Iniciante/C/main_1000.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/********************************************************
+SUBMISSÃO # 29074019
+    PROBLEMA: 1000 - Hello World!
+    RESPOSTA: Accepted
+    LINGUAGEM: C99 (gcc 4.8.5, -std=c99 -O2 -lm) [+0s]
+    TEMPO: 0.000s
+    TAMANHO: 84 Bytes
+    MEMÓRIA: -
+    SUBMISSÃO: 01/08/2022 23:46:25
+*********************************************************/
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
