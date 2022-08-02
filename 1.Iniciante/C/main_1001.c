1305c442af006419bdc3409371e1e17eab4fb96f
Cleo Aguiar <cleotaisas@gmail.com>
2022-08-01 21:10:18 -0300

    feat: PROBLEMA: 1001 - Extremamente Básico

---
+++ 1.Iniciante/C/main_1001.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/********************************************************
+SUBMISSÃO # 29074218
+    PROBLEMA: 1001 - Extremamente Básico
+    RESPOSTA: Accepted
+    LINGUAGEM: C99 (gcc 4.8.5, -std=c99 -O2 -lm) [+0s]
+    TEMPO: 0.000s
+    TAMANHO: 126 Bytes
+    MEMÓRIA: -
+    SUBMISSÃO: 02/08/2022 00:06:23
+*********************************************************/
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("X = %d\n", a+b);
+    return 0;
+}
