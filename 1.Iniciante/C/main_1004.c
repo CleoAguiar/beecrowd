90bd83d70a6212353ccf8a8f249e03741a8f550b
Cleo Aguiar <cleotaisas@gmail.com>
2022-08-01 21:42:02 -0300

    PROBLEMA: 1004 - Produto Simples

---
+++ 1.Iniciante/C/main_1004.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/********************************************************
+SUBMISSÃO # 29074641
+    PROBLEMA: 1004 - Produto Simples
+    RESPOSTA: Accepted
+    LINGUAGEM: C99 (gcc 4.8.5, -std=c99 -O2 -lm) [+0s]
+    TEMPO: 0.000s
+    TAMANHO: 254 Bytes
+    MEMÓRIA: -
+    SUBMISSÃO: 02/08/2022 00:40:00
+*********************************************************/
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("PROD = %d\n", a*b);
+    return 0;
+}
