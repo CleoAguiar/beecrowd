03aeca58da062f2c313e006cae340578e192cb36
Cleo Aguiar <cleotaisas@gmail.com>
2022-08-01 21:38:38 -0300

    PROBLEMA: 1003 - Soma Simples

---
+++ 1.Iniciante/C/main_1003.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/********************************************************
+SUBMISSÃO # 29074616
+    PROBLEMA: 1003 - Soma Simples
+    RESPOSTA: Accepted
+    LINGUAGEM: C99 (gcc 4.8.5, -std=c99 -O2 -lm) [+0s]
+    TEMPO: 0.000s
+    TAMANHO: 252 Bytes
+    MEMÓRIA: -
+    SUBMISSÃO: 02/08/2022 00:37:22
+*********************************************************/
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("SOMA = %d\n", a+b);
+    return 0;
+}
